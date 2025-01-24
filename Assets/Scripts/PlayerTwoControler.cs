d2bf4d9dacab7c66c2523560db0124716d9866b9
SpeakerLive <62421000+SpeakerLive@users.noreply.github.com>
2025-01-24 21:12:18 +0100

    Player Controller inherence

--- Assets/Scripts/PlayerTwoControler.cs
+++ Assets/Scripts/PlayerTwoControler.cs
@@ -1,7 +1,7 @@
 
 using UnityEngine;
 
-public class PlayerTwoControler : MonoBehaviour
+public class PlayerTwoControler : PlayerControler
 {
     [SerializeField] private PlayerTwoInputReader inputReader;
     [SerializeField] private PlayerSettings playerSettings;
