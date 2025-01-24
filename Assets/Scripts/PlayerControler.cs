1f651a6db91b9bc9357389f56952afb8750d4594
SpeakerLive <62421000+SpeakerLive@users.noreply.github.com>
2025-01-24 21:39:50 +0100

    changed rigidbody to use force

--- Assets/Scripts/PlayerControler.cs
+++ Assets/Scripts/PlayerControler.cs
@@ -2,5 +2,36 @@ using UnityEngine;
 
 public class PlayerControler : MonoBehaviour
 {
+    [SerializeField] protected PlayerSettings playerSettings;
     
+    [SerializeField] protected Rigidbody2D rb2D;
+    
+    [SerializeField] protected bool isGrounded;
+    [SerializeField] protected bool jumpRequested;
+    [SerializeField] protected bool actionRequested;
+    [SerializeField] protected bool isJumping;
+    [SerializeField] protected LayerMask groundLayer;
+    [SerializeField] protected Transform groundCheck;
+    
+    protected Vector2 movementVectorLeftRight;
+    protected float currentHorizontalSpeed;
+    protected float currentVerticalSpeed;
+    protected float timeInAirAfterUngrounded;
+
+
+    protected void HandleOnMoveLeftRight(Vector2 value)
+    {
+        Debug.Log(value);
+        movementVectorLeftRight = value;
+    }
+
+    protected void HandleJump(bool value)
+    {
+        isJumping = value;
+    }
+
+    protected void HandleAction(bool value)
+    {
+        actionRequested = value;
+    }
 }
