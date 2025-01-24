8a8846c10d65c99fd3e65760ee8e337f6e81b390
Maksymilian Zając <maksymilianzajac18@gmail.com>
2025-01-24 22:30:37 +0100

    Fix BoosterAnyDirection

--- Assets/Scripts/Booster/PlayerBoosterAnyDirection.cs
+++ Assets/Scripts/Booster/PlayerBoosterAnyDirection.cs
@@ -15,7 +15,7 @@ public class PlayerBoosterAnyDirection : MonoBehaviour
     private void BoostPlayer(GameObject Player)
     {
         var PlayerRigidbody2D = Player.GetComponent<Rigidbody2D>();
-        var Direction = transform.position - Player.transform.position;
+        var Direction = PlayerRigidbody2D.linearVelocity;
         PlayerRigidbody2D.AddForce(Direction.normalized * _boosterStrength, ForceMode2D.Impulse);
     }
 }
