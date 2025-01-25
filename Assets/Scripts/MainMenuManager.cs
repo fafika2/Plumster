900d563600279b7c77d5519d837d5a9f13838275
SpeakerLive <62421000+SpeakerLive@users.noreply.github.com>
2025-01-25 10:50:35 +0100

    Added TMP and created MainMenu

---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenuManager : MonoBehaviour
+{
+    [SerializeField] private string SceneToLoad;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            SceneManager.LoadScene(SceneToLoad);
+        }
+        
+    }
+}
