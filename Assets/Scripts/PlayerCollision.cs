09df2efac73221c80932706667cd5d03f5540f97
SpeakerLive <62421000+SpeakerLive@users.noreply.github.com>
2025-01-25 10:06:51 +0100

    Update PlayerCollision.cs

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -38,7 +38,7 @@ public class PlayerCollision : MonoBehaviour
 
                 Vector2 cachedVelocityPlayer1 = playerVelocity.normalized;
                 float differenceInVelocity = sumUpVelocityEnemy / sumUpVelocityPlayer;
-                float roundedFloat = Mathf.Round(differenceInVelocity * 10f) * 0.1f;
+                float roundedFloat = Mathf.Round(differenceInVelocity * 100f) * 0.1f;
                 Debug.Log($"Player has higher velocity {differenceInVelocity}");
                 
                 other.GetComponent<Rigidbody2D>().linearVelocity = Vector2.zero;
@@ -64,6 +64,21 @@ public class PlayerCollision : MonoBehaviour
 
                 other.GetComponent<Rigidbody2D>().linearVelocity = Vector2.zero;
                 other.GetComponent<Rigidbody2D>().AddForce(cachedVelocityPlayer1 * -1 * playerSettings.CollisionPower * roundedFloat, ForceMode2D.Impulse);
+                other.GetComponent<Rigidbody2D>().AddForce(Vector2.up  * 500, ForceMode2D.Impulse);
+            }
+            else
+            {
+                Vector2 cachedVelocityPlayer1 = new Vector2 (Mathf.Sign(playerVelocityX), Mathf.Sign(playerVelocityY));
+                float differenceInVelocity = sumUpVelocityEnemy / sumUpVelocityPlayer;
+                float roundedFloat = Mathf.Round(differenceInVelocity * 10f) * 0.1f;
+                Debug.Log($"Enemy has higher velocity {differenceInVelocity}");
+
+                player.GetComponent<Rigidbody2D>().linearVelocity = Vector2.zero;
+                player.GetComponent<Rigidbody2D>().AddForce(cachedVelocityPlayer1 * playerSettings.CollisionPower, ForceMode2D.Impulse);
+                player.GetComponent<Rigidbody2D>().AddForce(Vector2.up  * 500, ForceMode2D.Impulse);
+
+                other.GetComponent<Rigidbody2D>().linearVelocity = Vector2.zero;
+                other.GetComponent<Rigidbody2D>().AddForce(cachedVelocityPlayer1 * -1 * playerSettings.CollisionPower, ForceMode2D.Impulse);
                 other.GetComponent<Rigidbody2D>().AddForce(Vector2.up  * 500, ForceMode2D.Impulse);
             }
             
