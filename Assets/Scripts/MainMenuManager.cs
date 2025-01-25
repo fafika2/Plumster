261adc8161ffcbc8dbca19717c274868da667dbf
Maksymilian Zając <maksymilianzajac18@gmail.com>
2025-01-25 19:19:03 +0100

    new arena

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -5,14 +5,20 @@ public class MainMenuManager : MonoBehaviour
     [SerializeField] private string SceneToLoad;
 
     [SerializeField] private SO_Score score;
+    [SerializeField] private Animator _fadeInAnimator;
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.P))
         {
-            SceneManager.LoadScene(SceneToLoad);
-            score.ResetScore();
+            _fadeInAnimator.SetBool("FadeIn", true);
+            Invoke("LoadGame", 1f);
         }
-        
+    }
+
+    private void LoadGame()
+    {
+        score.ResetScore();
+        SceneManager.LoadScene(SceneToLoad);
     }
 }
